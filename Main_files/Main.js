750550b8d76beda4ffb2e39b568bbbfc69354b2c
Pablo Castro <pcastro@dc.exa.unrc.edu.ar>
2019-11-22 10:44:13 -0300

    main files added

---
+++ Main_files/Main.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20111123
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Main_files/Formal_inset_01.png'),IWCreateImage('Main_files/Formal_inset_02.png'),IWCreateImage('Main_files/Formal_inset_03.png'),IWCreateImage('Main_files/Formal_inset_06.png'),IWCreateImage('Main_files/Formal_inset_09.png'),IWCreateImage('Main_files/Formal_inset_08.png'),IWCreateImage('Main_files/Formal_inset_07.png'),IWCreateImage('Main_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Main_files/MainMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
